4ed15fccd5bf647a0ab48ef8021217fb479b8f54
Agustin Benvenuto <abenvenuto@itba.edu.ar>
2023-08-31 13:22:38 -0300

    ups I forget abou the agent 😵‍💫

--- TP-2/agent.py
+++ TP-2/agent.py
@@ -4,13 +4,21 @@ from role import CharacterStats, Role, ItemStats, Modifiers, get_attack, get_def
 class Agent:
     role: Role
     stats: CharacterStats
+    height: float
+    cromosome: list[float]
 
-    def __init__(self, role: Role, items: ItemStats):
+    def __init__(self, role: Role, cromosome: list):
+        self.cromosome = cromosome
+        items = ItemStats.from_weights(cromosome[0:-1])
+        self.height = cromosome[-1]
         self.role = role
         self.stats = CharacterStats(items)
 
-    def compute_performance(self, height: float) -> float:
-        modifiers: Modifiers = Modifiers(height)
+    def compute_performance(self) -> float:
+        """
+            fitness function
+        """
+        modifiers: Modifiers = Modifiers(self.height)
         attack: float = get_attack(self.stats, modifiers)
         defense: float = get_defense(self.stats, modifiers)
         return self.role.compute_performance(attack, defense)
