74133bdf7a320b13e136ed800287ae754defb91b
abrilocchipinti <aocchipinti@itba.edu.ar>
2023-09-10 10:04:51 -0300

    add plot all lines

--- TP-2/plot/main.py
+++ TP-2/plot/main.py
@@ -4,6 +4,7 @@ import pandas as pd
 from matplotlib import pyplot as plt
 
 
+
 def plot_lines(role, hash):  # misma config o sea mismo hash con dif fecha
     out_path = os.getcwd() + "/../out/"
     output_files = []
@@ -13,6 +14,7 @@ def plot_lines(role, hash):  # misma config o sea mismo hash con dif fecha
         file_path = os.path.join(out_path, filename)
         if os.path.isfile(file_path) and identifier in filename:
             output_files.append(file_path)
+
     # [csv, csv]
     for csv in output_files:
         csv_object = pd.read_csv(csv)
@@ -31,7 +33,6 @@ def plot_lines(role, hash):  # misma config o sea mismo hash con dif fecha
 def main():
     role = "Fighter"
     hash = "453bca740c30a03ac81476df14dbac9a"
-
 
     # plot_lines([out_path+"output_Fighter_453bca740c30a03ac81476df14dbac9a_2023-09-09-13-02-35.csv"])
     # print(output_files)
