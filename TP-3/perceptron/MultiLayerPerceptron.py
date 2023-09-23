0832713be62a6aa5eb33d83bb612aacd0edde039
khcatino <kcatino@itba.edu.ar>
2023-09-23 10:10:22 -0300

    Add batch, minibatch and online trainers

--- TP-3/perceptron/MultiLayerPerceptron.py
+++ TP-3/perceptron/MultiLayerPerceptron.py
@@ -3,12 +3,15 @@ from dense import Dense
 from typing import Type
 from errors import ErrorFunction,MeanSquared
 import numpy as np
+from trainer import Trainer, Batch, MiniBatch, Online
 
 
 
 class MultiLayerPerceptron:
     def __init__(self, layers: list[int], input_size: int, output_size: int,activation: Type[Activation]) -> None:
         layers = [input_size] + layers + [output_size]
+        self.input_size = input_size
+        self.output_size = output_size
         layer_list = []
         i = 0
         while i < (len(layers)-1):
@@ -26,10 +29,13 @@ class MultiLayerPerceptron:
             output = layer.forward(output)
         return output
 
-    def train(self, error_func: ErrorFunction, x_train, y_train, epochs = 1000, learning_rate = 0.01, verbose = True):
-        for e in range(epochs):
+    def train(self, error_func: ErrorFunction, x_train, y_train,training_method: Trainer,epochs = 1000, learning_rate = 0.01, verbose = True):
+        x_train = np.reshape(x_train,(len(x_train),self.input_size,1))
+        y_train = np.reshape(y_train,(len(y_train), self.output_size,1))
+
+        for e, dataset in enumerate(training_method.iterator(x_train, y_train, epochs)):
             error = 0
-            for x, y in zip(x_train, y_train):
+            for x, y in dataset:
                 output = self.predict(x)
 
                 error += error_func.eval(y, output)
@@ -46,12 +52,12 @@ class MultiLayerPerceptron:
 
         
 def main():
-    p = MultiLayerPerceptron([3], 2, 1,Sigmoid)
+    p = MultiLayerPerceptron([4], 2, 1,Sigmoid)
     print(p.predict([0,1]))
-    train_x = np.reshape([[0,0],[0,1],[1,0],[1,1]],(4,2,1))
-    train_y = np.reshape([[0],[1],[1],[0]],(4,1,1))
-    p.train(MeanSquared, train_x, train_y, 20000, 0.01, False)
-    print(p.predict([0,1]))
+    train_x = [[0,0],[0,1],[1,0],[1,1]]
+    train_y = [[0],[1],[1],[0]]
+    p.train(MeanSquared(), train_x, train_y, MiniBatch(2), 20000, 0.01, True)
+    print(p.predict([1,1]))
     
 
     
