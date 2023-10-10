dd36d47a67cbd11e0405f283ede317c739f53d35
Alberto <tataabancens@gmail.com>
2023-10-10 18:00:30 -0300

    Hopfield Working for letters

--- TP-4/Ej2/code/hopfield.py
+++ TP-4/Ej2/code/hopfield.py
@@ -6,9 +6,10 @@ from Ej2.code.letterParser import Letter, read_input_file
 
 class Hopfield:
 
-    def __init__(self, letters: list[Letter]):
+    def __init__(self, letters: list[Letter], num_iter):
         self.letters = letters
         self.W = self.train_weights()
+        self.num_iter = num_iter
 
     def train_weights(self) -> ndarray:
         patterns = []
@@ -25,8 +26,32 @@ class Hopfield:
 
         return mat_w
 
+    def run(self, pattern: ndarray):
+        s = pattern
+        prev = pattern
+        for i in range(self.num_iter):
+            print(i)
+            s = np.sign(self.W @ s)
+
+            if np.array_equal(s, prev):
+                return s
+            prev = s
+        return s
+
 
 if __name__ == "__main__":
-    letras = read_input_file("../input/simpleExample.json")
-    hopfield = Hopfield(letras)
+    letras = read_input_file("../input/pattern_letters_2.json")
+    hopfield = Hopfield(letras, 1000)
+
+    letter = letras[2]
+
+    noisy_letter = letter.copy_with_noise(0.25, 1)
+
+    pat = np.array(noisy_letter.data)
+    s_final = hopfield.run(pat)
+
+    final_letter = Letter("j", s_final)
+    print(letter)
+    print(noisy_letter)
+    print(final_letter)
 
--- TP-4/Ej2/code/letterParser.py
+++ TP-4/Ej2/code/letterParser.py
@@ -9,11 +9,44 @@ class Letter:
         self.name = name
         self.data = data
 
-    def __str__(self):
-        return self.name
-
     def __repr__(self):
-        return self.name
+        matriz_str = ""
+        for i in range(5):
+            for j in range(5):
+                index = i * 5 + j
+                if self.data[index] == 1:
+                    matriz_str += "* "
+                else:
+                    matriz_str += "  "
+            matriz_str += '\n'
+        return matriz_str
+
+    def copy_with_noise(self, noise: float, seed: int):
+        return Letter(self.name, self.apply_noise(self.data, noise, seed))
+
+    @staticmethod
+    def apply_noise(matriz: ndarray, noise_proportion: float, seed: int):
+        """
+        Aplica un ruido a la matriz cambiando aleatoriamente una proporción de bits.
+
+        Args:
+            matriz (numpy.ndarray): Matriz de entrada con valores 1 y -1.
+            noise_proportion (float): Proporción de bits que serán cambiados (0.0 a 1.0).
+            seed (int): Semilla para la generación de números aleatorios.
+        Returns:
+            numpy.ndarray: Matriz con el ruido aplicado.
+        """
+        np.random.seed(seed)
+        noisy_mat = np.copy(matriz)
+
+        total_bits = matriz.size
+        num_bits_to_shift = int(total_bits * noise_proportion)
+
+        indexes_to_change = np.random.choice(total_bits, num_bits_to_shift, replace=False)
+
+        noisy_mat[indexes_to_change] *= -1
+
+        return noisy_mat
 
 
 def read_input_file(filepath: str) -> list[Letter]:
